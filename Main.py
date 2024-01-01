a8a90f8eb0d2679db22872b4084cf2381e0a5e41
Aryan A <79328550+aryananumula@users.noreply.github.com>
2023-12-31 17:00:41 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,9 @@
+import json
+from Encoder import encode
+from Decoder import decode
 
+vocab = json.loads(open("vocab.json", "r").read())
+
+y = encode("Why are you like this", vocab)
+print(y)
+print(decode(y, vocab))
